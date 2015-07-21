0bcd25b4ecd35379d85b3f1927d31d8683e0d9f8
jonathan thornburg <reliable2192@gmail.com>
2015-07-21 14:10:30 -0400

    Initial Commit

---
+++ FindMyTrail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindMyTrail
+//
+//  Created by jonathan thornburg on 7/21/15.
+//  Copyright (c) 2015 jonathan thornburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
